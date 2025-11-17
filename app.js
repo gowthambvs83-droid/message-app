901cca2f1939c6aa95dc624d99210a76a8fd672b
gowthambvs83-droid <gowthambvs83@gmail.com>
2025-11-17 14:10:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,50 @@
 let userEmail = localStorage.getItem("email");
+const API = ""; // empty means same domain
 
-// LOGIN
 function login() {
   const email = document.getElementById("email").value;
 
-  fetch("http://localhost:3000/login", {
+  fetch(`${API}/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email })
   })
   .then(res => res.json())
   .then(data => {
-    if (data.success) {
-      localStorage.setItem("email", email);
-      window.location = "chat.html";
-    }
+    localStorage.setItem("email", email);
+    window.location = "chat.html";
   });
 }
 
-// IF IN CHAT PAGE
 if (window.location.pathname.includes("chat.html")) {
   document.getElementById("welcome").innerText = "Logged in as: " + userEmail;
   loadMessages();
 }
 
-// SEND MESSAGE
 function sendMsg() {
   const receiver = document.getElementById("receiver").value;
   const message = document.getElementById("msg").value;
 
-  fetch("http://localhost:3000/send", {
+  fetch(`${API}/send`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      sender: userEmail,
-      receiver: receiver,
-      message: message
-    })
+    body: JSON.stringify({ sender: userEmail, receiver, message })
   })
   .then(res => res.json())
-  .then(() => {
-    alert("Message Sent!");
-  });
+  .then(() => alert("Message Sent!"));
 }
 
-// LOAD INBOX
 function loadMessages() {
-  fetch(`http://localhost:3000/messages/${userEmail}`)
+  fetch(`${API}/messages/${userEmail}`)
     .then(res => res.json())
     .then(data => {
       const box = document.getElementById("messages");
       box.innerHTML = "";
 
       data.messages.forEach(msg => {
-        box.innerHTML += `
-          <p><b>${msg.sender}:</b> ${msg.message}</p>
-        `;
+        box.innerHTML += `<p><b>${msg.sender}:</b> ${msg.message}</p>`;
       });
     });
 
-  setTimeout(loadMessages, 3000); // auto refresh inbox
+  setTimeout(loadMessages, 3000);
 }
